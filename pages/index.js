845e5df83bcbb9f2dc2e0c42aa9438a66be623bb
Mhingzzz <nathaphong_p@cmu.ac.th>
2022-09-08 14:43:36 +0700

    finiish

--- pages/index.js
+++ pages/index.js
@@ -6,39 +6,49 @@ import Canvas from "../components/Canvas";
 import CanvasLib from "../libs/CanvasLib";
 
 export default function Home() {
-  //selected color from color picker
-  //set black color as default
-  const [selColor, setSelColor] = useState("#000000");
+	//selected color from color picker
+	//set black color as default
+	const [selColor, setSelColor] = useState("#000000");
 
-  //16x16 2D Array that holds color data
-  const [pixels, setPixels] = useState(CanvasLib.createEmptyCanvas());
+	//16x16 2D Array that holds color data
+	const [pixels, setPixels] = useState(CanvasLib.createEmptyCanvas());
 
-  //will be called by Cell component
-  const paint = (xPos, yPos) => {
-    //copy from old 2d Array
-    const newPixels = CanvasLib.copyCanvas(pixels);
-    //your code here
-  };
+	//will be called by Cell component
+	const paint = (xPos, yPos) => {
+		//copy from old 2d Array
+		const newPixels = CanvasLib.copyCanvas(pixels);
+		newPixels[xPos][yPos] = selColor;
+		setPixels(newPixels);
+		//update newPixels[...][...] xPos and yPos selColor
+		//setPixel(...)
+		//your code here
+	};
 
-  const clear = () => {
-    //your code here
-    //Hint : use CanvasLib.createEmptyCanvas()
-  };
+	const clear = () => {
+		//your code here
+		setPixels(CanvasLib.createEmptyCanvas());
+		//Hint : use CanvasLib.createEmptyCanvas()
+	};
 
-  return (
-    <div style={{ minHeight: "100vh", backgroundColor: "GhostWhite" }}>
-      <PainterContext.Provider value={{ selColor, setSelColor, pixels, paint }}>
-        <Header />
-        <ColorPickerContainer />
-        <Canvas />
+	const random = () => {
+		setPixels(CanvasLib.createRandomCanvas());
+	};
+	return (
+		<div style={{ minHeight: "100vh", backgroundColor: "GhostWhite" }}>
+			<PainterContext.Provider value={{ selColor, setSelColor, pixels, paint }}>
+				<Header />
+				<ColorPickerContainer />
+				<Canvas />
 
-        <div className="d-flex justify-content-center gap-2">
-          <button className="btn btn-dark" onClick={clear}>
-            Clear
-          </button>
-          <button className="btn btn-dark">Random Color</button>
-        </div>
-      </PainterContext.Provider>
-    </div>
-  );
+				<div className="d-flex justify-content-center gap-2">
+					<button className="btn btn-dark" onClick={clear}>
+						Clear
+					</button>
+					<button className="btn btn-dark" onClick={random}>
+						Random Color
+					</button>
+				</div>
+			</PainterContext.Provider>
+		</div>
+	);
 }
